84f62e55f72a68cec2e98630c23a74c901e5196f
JLetuce <01206505@pw.edu.pl>
2026-05-01 20:22:27 +0200

    added textureId in structure

--- src/world/structure.h
+++ src/world/structure.h
@@ -1,7 +1,12 @@
 #pragma once
+#include <string>
 
 class Structure {
+	private:
+		std::string textureId;
+
 	public:
 	virtual void init();
 	virtual void update(float dt) = 0;
+	virtual std::string getTextureId();
 };
