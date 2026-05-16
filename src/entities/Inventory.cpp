d472ad8aa3d6f129134ce69fce90f6e39e76e299
JLetuce <01206505@pw.edu.pl>
2026-05-16 19:56:34 +0200

    refactored inventory to be more universal (To use them in structures)

--- src/entities/Inventory.cpp
+++ src/entities/Inventory.cpp
@@ -2,10 +2,49 @@
 
 void Inventory::init() {
     items.clear();
+    maxCapacities.clear();
 }
 
-void Inventory::addResource(MaterialType type, int amount) {
+void Inventory::setMaxCapacity(MaterialType type, int capacity) {
+    maxCapacities[type] = capacity;
+    if (items.find(type) == items.end()) {
+        items[type] = 0;
+    }
+}
+
+int Inventory::getMaxCapacity(MaterialType type) const {
+    auto it = maxCapacities.find(type);
+    if (it != maxCapacities.end()) {
+        return it->second;
+    }
+    return 0;
+}
+
+bool Inventory::addResource(MaterialType type, int amount) {
+    if (amount < 0) {
+        items[type] += amount;
+        return true;
+    }
+
+    int current = getAmount(type);
+    int max = getMaxCapacity(type);
+
+    if (current + amount > max) {
+        return false;
+    }
+
     items[type] += amount;
+    return true;
+}
+
+bool Inventory::subResource(MaterialType type, int amount) {
+    if (amount <= 0) return false;
+
+    if (!hasResource(type, amount)) {
+        return false;
+    }
+
+    return addResource(type, -amount);
 }
 
 bool Inventory::hasResource(MaterialType type, int amount) const {
--- src/entities/Inventory.h
+++ src/entities/Inventory.h
@@ -5,14 +5,18 @@
 class Inventory {
 private:
     std::unordered_map<MaterialType, int> items;
+    std::unordered_map<MaterialType, int> maxCapacities;
 
 public:
     void init();
 
-    void addResource(MaterialType type, int amount); // Change to bool if we want to check capacity!
-	bool subResource(MaterialType type, int amount); // TODO : implement this using addResource with negative amount, also check if the amount to subtract is available before, return true if the operation was successful, false otherwise
-    bool hasResource(MaterialType type, int amount) const;
+    void setMaxCapacity(MaterialType type, int capacity);
+    int getMaxCapacity(MaterialType type) const;
 
+    bool addResource(MaterialType type, int amount); // Change to bool if we want to check capacity!
+	bool subResource(MaterialType type, int amount); // TODO : implement this using addResource with negative amount, also check if the amount to subtract is available before, return true if the operation was successful, false otherwise
+    
+    bool hasResource(MaterialType type, int amount) const;
     int getAmount(MaterialType type) const;
 
     void clear();
